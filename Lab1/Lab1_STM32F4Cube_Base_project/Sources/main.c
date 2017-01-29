f3cab84fb57a9e4f1f9ac86e61bc451d30ec3757
Luis Gallet <nanogallet@gmail.com>
2017-01-29 15:06:25 -0500

    cambios antes del conflict

--- Lab1/Lab1_STM32F4Cube_Base_project/Sources/main.c
+++ Lab1/Lab1_STM32F4Cube_Base_project/Sources/main.c
@@ -3,14 +3,20 @@
 
 typedef struct   
 {
-	float b0;
-	float b1;
-	float b2;
-	float b3;
-	float b4;
+	int coeff_lenght;
+	float b[];
 }FIR_coeff;
 
 int FIR_C(float* InputArray, float* OutputArray, FIR_coeff* coeff, int Length, int Order){
+		
+	for(int i = Length - 1; i >= Order; i--){
+		OutputArray[i] = (coeff->b0 * InputArray[i])+ 
+											(coeff->b1 * InputArray[i - (Order - 3)])+ 
+												(coeff->b2 * InputArray[i - (Order - 2)])+ 
+													(coeff->b3 * InputArray[i - (Order - 1)])+ 
+														(coeff->b4 * InputArray[i - (Order)]);
+		
+	}
 	
 	return 0;
 }
@@ -19,6 +25,9 @@ int FIR_C(float* InputArray, float* OutputArray, FIR_coeff* coeff, int Length, i
 
 int main()
 {
+	FIR_coeff coeff;
+	
+	coeff.b[] = {0.1, 0.15, 0.5, 0.15, 0.1};
 	
 	return 0;
 }
