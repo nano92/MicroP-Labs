52a686bf0e4875e03a2d35fd1cc450f35f6cf741
Luis Gallet <nanogallet@gmail.com>
2017-01-29 16:26:53 -0500

    FIR_C casi terminada

--- Lab1/Lab1_STM32F4Cube_Base_project/Sources/main.c
+++ Lab1/Lab1_STM32F4Cube_Base_project/Sources/main.c
@@ -1,40 +1,32 @@
 #include <stdio.h>
 #include "arm_math.h"
 
-typedef struct   
-{
-	int coeff_lenght;
-	float b[];
-}FIR_coeff;
-
-int FIR_C(float* InputArray, float* OutputArray, FIR_coeff* coeff, int Length, int Order){
-		
-	for(int i = Length - 1; i >= Order; i--){
-		OutputArray[i] = (coeff->b0 * InputArray[i])+ 
-											(coeff->b1 * InputArray[i - (Order - 3)])+ 
-												(coeff->b2 * InputArray[i - (Order - 2)])+ 
-													(coeff->b3 * InputArray[i - (Order - 1)])+ 
-														(coeff->b4 * InputArray[i - (Order)]);
-		
-	}
-	
-	return 0;
-}
-
-float sumArray(float* InputArray, FIR_coeff* coeff, int Order, int Location) {
+float Sum_Array(float* InputArray, float* FIR_coeff, int Order, int Location) {
 	float sum = 0;
-	for(int j = Order; j>=0; j--) {
-		sum = sum + InputArray[Location-j]*coeff->b[j];
+	for(int j = Order; j >= 0; j--) {
+		sum = sum + InputArray[Location - j] * (FIR_coeff[j]);
 	}		
 	return sum;
 }
 
+int FIR_C(float* InputArray, float* OutputArray, float* FIR_coeff, int Length, int Order){
+		
+	for(int i = Length - 1; i >= Order; i--){
+		OutputArray[i] = Sum_Array(InputArray, FIR_coeff, Order, i);
+	}
+		
+	return 0;
+}
+
+int FIR_CMSIS(float* InputArray, float* OutputArray, float* FIR_coeff, int Length, int Order){
+	arm_fir_instance_f32 S;
+	
+	return 0;
+}
+
 int main()
 {
-
-	FIR_coeff coeff;
-	
-	coeff.b[] = {0.1, 0.15, 0.5, 0.15, 0.1};
+	float FIR_coeff[5] = {0.1, 0.15, 0.5, 0.15, 0.1};	
 	
 
 	return 0;
