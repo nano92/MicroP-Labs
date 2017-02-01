ff82d2423ee833fe71704508e7f801268a565dcb
nano92 <nanogallet@gmail.com>
2017-02-01 15:32:04 -0500

    FIR_C y FIR_ASM funcionan, falta testear FIR_CMSIS. Parece que tienes problemas

--- Lab1/Lab1_STM32F4Cube_Base_project/Sources/Assembly.s
+++ Lab1/Lab1_STM32F4Cube_Base_project/Sources/Assembly.s
@@ -3,62 +3,35 @@
 	EXPORT FIR_asm
 	
 FIR_asm	
-	PUSH	{R4-R12}
-	VPUSH    {S0-S12}
-	; OK tenemos un pequeno problema, ya que el loading del float point no se puede hacer con los registers R apparentemente
-	; Hay que conseguir una forma de modificar eso
-	; Hay que tambien cambiar un poco el agorithmo
-	ADD R0, R0, #16    ; Starts the filtering at the Order-th input
-	VLDM.f32 R3, {S8-S12}
-	SUB R2, R2, #4
-	VLDM.f32 R0, {S1-S5}	;Load fisrt batch of inputs
-fdat
-	VMUL.f32 S0, S5, S12    ;Multiplies X[n-4] by b4
-	VMLA.f32 S0, S4, S11 	;Multiplies X[n-3] by b3 and adds to the sum
-	VMOV.f32 S5, S4         ;Shift X[n-3] to X[n-4]
-	VMLA.f32 S0, S3, S10 	;Multiplies X[n-2] by b2 and adds to the sum
-	VMOV.f32 S4, S3         ;Shift X[n-2] to X[n-3]
-	VMLA.f32 S0, S2, S9  	;Multiplies X[n-1] by b1 and adds to the sum
-	ADD R0, R0, #4			;Points to next input value
-	VMOV.f32 S3, S2			;Shift X[n-1] to X[n-2]
-	VMLA.f32 S0, S1, S8		;Multiplies X[n] by b0 and adds to the sum
-	SUB R2, R2, #1			;Generates the exit condition
-	VMOV.f32 S2, S1			;Shift X[n] to X[n-1]
-	VLDR.f32 S1, [R0]		;Load new input
-	CMP R2, #0
-	BEQ fdat
+	PUSH			{R4-R12}
+	VPUSH			{S0-S12}
+	
+	VLDM.f32	R3, {S8-S12}	;Load coefficients 
+	SUB 			R2, R2, #4		;Skipped first five elements, Length - Order
+	VLDM.f32 	R0, {S1-S5}		;Load first batch of inputs
+	ADD 		 	R0, R0, #20		;Points to next input value, after the first filter pass
 
-;####################################################################
-;	SUB	  	R0, R0, #4	  	;Points to X[n-1]
-;	VLDR.f32 S1, [R0]	    ;Loads X[n-1] value
-;	VMUL.f32 S0, S0, S8 	;Multiplies X[n] with b0
-;	
-;	SUB		R0, R0, #4	  	;Points to  X[n-2]
-;	VMUL.f32 S1, S1, S9 	;Multiplies X[n-1] with b1
-;	VLDR.f32 S2, [R0]		;Loads X[n-2] value
-;	VADD.f32 S0, S0, S1 	;Adds X[n-1]*b1 to Sum
-;	
-;	SUB		R0, R0, #4	  	;Points to  X[n-3]
-;	VMUL.f32 S2, S2, S10 	;Multiplies X[n-2] with b2
-;	VLDR.f32 S1, [R0]		;Loads X[n-3] value
-;	VADD.f32 S0, S0, S2 	;Adds X[n-2]*b2 to Sum
-;	
-;	SUB		R0, R0, #4	  	;Points to  X[n-3]
-;	VMUL.f32 S1, S1, S11 	;Multiplies X[n-3] with b3
-;	VLDR.f32 S1, [R0]		;Loads X[n-4] value
-;	VADD.f32 S0, S0, S1 	;Adds X[n-3]*b3 to Sum
-;	
-;	SUB		R0, R0, #4	  	;Points to  X[n-4]
-;	VMUL.f32 S1, S1, S12 	;Multiplies X[n-4] with b4	
-;	SUB   	R2, R2, #4		;Decreases the length of for loop	
-;	VADD.f32 S0, S0, S1 	;Adds X[n-4]*b4 to Sum
-;	
-;	VSTR.f32 S0, [R1]    	;Stores the value of the sum into Y[n]
-;	ADD   	R0, R0, #20 	;Points to X[n]
-;	ADD   	R1, R1, #4	
-;	BNE   	for1 	        ;Check if loop length is Length - Order (It's multiplied by 4 for memory alignment)
-	;Probably a stall occurs here
-	VPOP     {S0-S12}
+loop
+	;Assuming n = 0
+	VMUL.f32 	S0, S1, S8  	;Multiplies X[n] by b0
+	VMLA.f32 	S0, S2, S9 		;Multiplies X[n+1] by b1 and adds to the sum
+	VMLA.f32 	S0, S3, S10 	;Multiplies X[n+2] by b2 and adds to the sum
+	VMOV.f32 	S1, S2       	;Shift X[n+1] to X[n]
+	VMLA.f32 	S0, S4, S11  	;Multiplies X[n+3] by b3 and adds to the sum
+	VMOV.f32 	S2, S3       	;Shift X[n+2] to X[n+1]
+	SUBS 		 	R2, R2, #1		;Generates the exit condition. Set Z(zero) flag high if R2 is zero
+	VMOV.f32 	S3, S4				;Shift X[n+3] to X[n+2]
+	VMLA.f32 	S0, S5, S12		;Multiplies X[n+4] by b4 and adds to the sum
+	VMOV.f32 	S4, S5				;Shift X[n+4] to X[n+3]
+	VSTR.f32	S0, [R1]			;Stores value into outputs at index R1
+	VLDR.f32 	S5, [R0]			;Load new input at X[n+4]
+	ADD				R1, R1, #4		;Points to next output index
+	VSUB.f32	S0, S0, S0		;Reset Sum
+	ADD 		 	R0, R0, #4		;Points to next input value
+	BNE 			loop					;Loop if R2 is not zero
+		
+
+	VPOP	{S0-S12}
 	POP		{R4-R12}
 	BX		lr
 	END 
--- Lab1/Lab1_STM32F4Cube_Base_project/Sources/main.c
+++ Lab1/Lab1_STM32F4Cube_Base_project/Sources/main.c
@@ -6,16 +6,16 @@ extern int FIR_asm();
 
 int32_t FIR_C(float32_t* InputArray, float32_t* OutputArray, float32_t* FIR_coeff, int32_t Length, int32_t Order){
 	
-	for(int32_t i = Order; i <= Length - 1; i++){ 
+	for(int32_t i = 0; i < Length - Order; i++){ 
 		float32_t sum = 0;
 		for(int32_t j = 0; j <= Order; j++){
 			//printf("Input[%d] = %.4f\n",i - j,InputArray[i-j]);
 			//printf("j = %d\n", j);
 			//printf("Coeff[%d] = %.4f\n",j,FIR_coeff[j]);
-			sum = sum  + ((InputArray[i - j]) * (FIR_coeff[j]));
+			sum = sum  + ((InputArray[i + j]) * (FIR_coeff[j]));
 		}
 		//printf("Loop finished");
-		OutputArray[i-Order] = sum;
+		OutputArray[i] = sum;
 	}
 	return 0;
 }
@@ -30,7 +30,7 @@ int32_t FIR_CMSIS(float32_t* InputArray, float32_t* OutputArray, float32_t* FIR_
 	
 	float32_t FIR_state[Length + Order];
 	
-	arm_fir_init_f32(&S, Order, &(FIR_coeff[0]), &(FIR_state[0]), Length);
+	arm_fir_init_f32(&S, Order, FIR_coeff, FIR_state, Length);
 	
 	arm_fir_f32(&S, InputArray, OutputArray, Length);
 	
@@ -42,7 +42,8 @@ int32_t testbench()
 	srand(time(NULL));
 	
 	float32_t FIR_coeff[5] = {0.1, 0.15, 0.5, 0.15, 0.1};	
-	float32_t InputArray[10];
+	float32_t InputArray[10]; //= {0.5, 0.9, 0.34, 0.69, 0.34, 0.12, 0.89};
+	float32_t OutputArray_ASM[10];
 	float32_t OutputArray_C[10];
 	float32_t OutputArray_CMSIS[100];
 	for(int32_t i = 0; i < 10; i++){
@@ -57,13 +58,13 @@ int32_t testbench()
 		//printf("InputArray[%d] = %.4f\n", r, InputArray[r]);
 	}
 	
-	FIR_ASM(InputArray, OutputArray_C, FIR_coeff, 10, 4);
+	FIR_ASM(InputArray, OutputArray_ASM, FIR_coeff, 10, 4);
 	
-	//FIR_C(InputArray, OutputArray_C, FIR_coeff, 100, 4);
-	//FIR_CMSIS(InputArray, OutputArray_CMSIS, FIR_coeff, 100, 4);
+	FIR_C(InputArray, OutputArray_C, FIR_coeff, 10, 4);
+	//FIR_CMSIS(InputArray, OutputArray_CMSIS, FIR_coeff, 10, 4);
 	
-	for(int i = 0; i < 100; i++){
-		printf("OA_C[%d] = %.4f\tOA_CMSIS[%d] = %.4f\n", i, OutputArray_C[i], i, OutputArray_CMSIS[i]);
+	for(int i = 0; i < 10; i++){
+		printf("OA_C[%d] = %.4f\tOA_ASM[%d] = %.4f\tOA_CMSIS[%d] = %.4f\n", i, OutputArray_C[i], i, OutputArray_ASM[i], i, OutputArray_CMSIS[i]);
 	}
 	
 	
