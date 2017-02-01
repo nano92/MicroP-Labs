539db86764f2a630ce7afa71f33430c22e274770
Juan-Ca <juan.borges@mail.mcgill.ca>
2017-01-31 23:14:48 -0500

    Cambio en los registros usados para el floating point

--- Lab1/Lab1_STM32F4Cube_Base_project/Sources/Assembly.s
+++ Lab1/Lab1_STM32F4Cube_Base_project/Sources/Assembly.s
@@ -4,50 +4,41 @@
 	
 FIR_asm	
 	PUSH	{R4-R12}
-	LDM 	R3, {R8-R12}
-	
-	;En vez de multiplicar por 4 debemos hacer un shift, es mas rapido
-	
-	MOV		R3, #4
-	MUL		R2, R2, R3	;Length of for loop in C needs to be multiplied by 4, for memory alignment
-	SUB		R2, R2, #4	;Index of last input element
-	MUL   R1, R1, R3	;Calculate offset for index of output's last element
-	SUB   R1, R1, #24 ;Index of last output element
-	ADD		R0, R0, R2	;Input's last element address
-	LDR		R3, [R0]		;Load input's last element value
+	; OK tenemos un pequeno problema, ya que el loading del float point no se puede hacer con los registers R apparentemente
+	; Hay que conseguir una forma de modificar eso
+	; Hay que tambien cambiar un poco el agorithmo
+	LDM 	R3, {S8-S12}
+	VMOV.f32 S0, [R0]		;Load input's last element value
 for1
-	SUB		R0, R0, #4	  ;Points to X[n-1]
-	LDR	  R4, [R0]	    ;Loads X[n-1] value
-	VMUL.f32 R3, R3, R8 ;Multiplies X[n] with b0
-	SUB		R0, R0, #4	  ;Points to  X[n-2]
-	VMUL.f32 R4, R4, R9 ;Multiplies X[n-1] with b1
+	SUB	  	R0, R0, #4	  	;Points to X[n-1]
+	VMOV.f32 S1, [R0]	    ;Loads X[n-1] value
+	VMUL.f32 S0, S0, S8 	;Multiplies X[n] with b0
 	
-	VADD.f32 R3, R3, R4 ;Adds X[n-1]*b1 to Sum
-	LDR	  R4, [R0]			;Loads X[n-2] value
-	SUB		R0, R0, #4	  ;Points to  X[n-3]
-	VMUL.f32 R4, R4, R10 ;Multiplies X[n-2] with b2
-
-	VADD.f32 R3, R3, R4 ;Adds X[n-2]*b2 to Sum
-	LDR	  R4, [R0]			;Loads X[n-3] value
-	SUB		R0, R0, #4	  ;Points to  X[n-3]
-	VMUL.f32 R4, R4, R11 ;Multiplies X[n-3] with b3
-
-	VADD.f32 R3, R3, R4 ;Adds X[n-3]*b3 to Sum
-	LDR	  R4, [R0]			;Loads X[n-4] value
-	SUB		R0, R0, #4	  ;Points to  X[n-4]
-	VMUL.f32 R4, R4, R12 ;Multiplies X[n-4] with b4
+	SUB		R0, R0, #4	  	;Points to  X[n-2]
+	VMUL.f32 S1, S1, S9 	;Multiplies X[n-1] with b1
+	VMOV.f32 S2, [R0]		;Loads X[n-2] value
+	VADD.f32 S0, S0, S1 	;Adds X[n-1]*b1 to Sum
 	
-	SUB   R2, R2, #4	;Decrease length of for loop
+	SUB		R0, R0, #4	  	;Points to  X[n-3]
+	VMUL.f32 S2, S2, S10 	;Multiplies X[n-2] with b2
+	VMOV.f32 S1, [R0]		;Loads X[n-3] value
+	VADD.f32 S0, S0, S2 	;Adds X[n-2]*b2 to Sum
 	
-	VADD.f32 R3, R3, R4 ;Adds X[n-4]*b4 to Sum
+	SUB		R0, R0, #4	  	;Points to  X[n-3]
+	VMUL.f32 S1, S1, S11 	;Multiplies X[n-3] with b3
+	VMOV.f32 S1, [R0]		;Loads X[n-4] value
+	VADD.f32 S0, S0, S1 	;Adds X[n-3]*b3 to Sum
 	
+	SUB		R0, R0, #4	  	;Points to  X[n-4]
+	VMUL.f32 S1, S1, S12 	;Multiplies X[n-4] with b4	
+	SUB   	R2, R2, #4		;Decreases the length of for loop	
+	VADD.f32 S0, S0, S1 	;Adds X[n-4]*b4 to Sum
 	
-	STR   R3, [R1]    ;Stores the value of the sum into Y[n]
-	ADD   R0, R0, #16 ;Points to X[n]
-	SUB   R1, R1, #4
-	
-	BNE   R2, #16, for1 ;Check if loop length is Length - Order (It's multiplied by 4 for memory alignment)
-	
+	STR   	S0, [R1]    	;Stores the value of the sum into Y[n]
+	SUB   	R0, R0, #16 	;Points to X[n]
+	ADD   	R1, R1, #4	
+	BNE   	R2, #16, for1 	;Check if loop length is Length - Order (It's multiplied by 4 for memory alignment)
+	;Probably a stall occurs here
 	POP		{R4-R12}
 	BX		lr
 	END 
